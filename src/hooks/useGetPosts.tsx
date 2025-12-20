33b29d61484ade35915e15750c9e286f5cba4815
musab <malqarninet@outlook.com>
2025-12-20 10:00:19 +0300

    install the required packages eslint for tanstack query and eslint for autocomplete for tansatck keys and installationa and setup for the stack

--- src/hooks/useGetPosts.tsx
+++ src/hooks/useGetPosts.tsx
@@ -3,7 +3,8 @@ import
 
 const useGetPosts = () => {
   return (
- 
+ <>
+ </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// QueryClientProvider such as provider of routing or redux stae management should be at the top level
+// to ensure that all components have access to the query client context.
+// QueryClient is responsible for managing the cache and state of your queries.
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Main from "./layouts/Main";
 import Home from "./pages/Home";
-import Create from "./pages/Create";
+// import Create from "./pages/Create";
 import Info from "./pages/Info";
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,10 +22,10 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
-      {
-        path: "/create",
-        element: <Create />,
-      },
+      // {
+      //   path: "/create",
+      //   element: <Create />,
+      // },
       {
         path: "/info",
         element: <Info />,
@@ -30,5 +35,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
