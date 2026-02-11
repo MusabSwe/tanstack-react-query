f2e2932d41dda7715d2873e696bff9f78142c6ac
musab <malqarninet@outlook.com>
2026-02-11 17:15:49 +0300

    - TanStack query: 13 more about cashing - TanStack query: 14 auto and manual refresh

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -8,7 +8,7 @@ interface PostListProps {
     searchQuery: string;
 }
 const PostList = ({ selectedPostStatus, searchQuery }: PostListProps) => {
-    const { data, isLoading, isError, error } = useGetPosts(selectedPostStatus);
+    const { data, isLoading, isError, error, isStale, refetch, isFetching } = useGetPosts(selectedPostStatus);
     const { data: searchData, isLoading: isSearchLoading, isError: isSearchError, error: searchError } = useSearch(searchQuery);
 
     // Handle loading and error states and if it not exist will crash since postsData is undefined 
@@ -24,51 +24,59 @@ const PostList = ({ selectedPostStatus, searchQuery }: PostListProps) => {
         return <div>Error loading posts {searchError?.message}</div>
     }
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Status</th>
-                    <th style={{ width: "10%" }}>Top Rate</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {searchQuery.length === 0 &&
-                    data?.map((post: any, index: number) => (
-                        <tr key={post.id}>
-                            <td>{index + 1}</td>
-                            <td><Link to={"/info"}>{post.title}</Link></td>
-                            <td style={{ textAlign: 'center' }}>
-                                <Form.Check type="switch" onChange={() => { }} checked={post.topRate} />
-                            </td>
-                            <td>
-                                <ButtonGroup aria-label="Basic example">
-                                    <Button variant="danger">Delete</Button>
-                                </ButtonGroup>
-                            </td>
-                        </tr>
-                    ))
-                }
-                {searchQuery.length > 0 &&
-                    searchData?.map((post: any, index: number) => (
-                        <tr key={post.id}>
-                            <td>{index + 1}</td>
-                            <td><Link to={"/info"}>{post.title}</Link></td>
-                            <td style={{ textAlign: 'center' }}>
-                                <Form.Check type="switch" onChange={() => { }} checked={post.topRate} />
-                            </td>
-                            <td>
-                                <ButtonGroup aria-label="Basic example">
-                                    <Button variant="danger">Delete</Button>
-                                </ButtonGroup>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </Table>
+        <>
+            {/* Manual update */}
+            {isStale && searchQuery.length === 0 &&
+                <Button onClick={() => refetch()}>
+                    Update Data
+                </Button>}
+            {isFetching && <span>Updating...</span>}
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Status</th>
+                        <th style={{ width: "10%" }}>Top Rate</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchQuery.length === 0 &&
+                        data?.map((post: any, index: number) => (
+                            <tr key={post.id}>
+                                <td>{index + 1}</td>
+                                <td><Link to={"/info"}>{post.title}</Link></td>
+                                <td style={{ textAlign: 'center' }}>
+                                    <Form.Check type="switch" onChange={() => { }} checked={post.topRate} />
+                                </td>
+                                <td>
+                                    <ButtonGroup aria-label="Basic example">
+                                        <Button variant="danger">Delete</Button>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                    {searchQuery.length > 0 &&
+                        searchData?.map((post: any, index: number) => (
+                            <tr key={post.id}>
+                                <td>{index + 1}</td>
+                                <td><Link to={"/info"}>{post.title}</Link></td>
+                                <td style={{ textAlign: 'center' }}>
+                                    <Form.Check type="switch" onChange={() => { }} checked={post.topRate} />
+                                </td>
+                                <td>
+                                    <ButtonGroup aria-label="Basic example">
+                                        <Button variant="danger">Delete</Button>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
+        </>
     )
 }
 
